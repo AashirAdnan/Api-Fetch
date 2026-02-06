39ed894e69e63527753c2d98ac1f16b099739f4f
aashir adnan <m.aashiradnan@gmail.com>
2026-02-06 10:45:48 +0500

    New React

---
+++ src/ApiFetch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ApiFetch = () => {
+  return (
+    <>
+      <div>ApiFetch</div>
+    </>
+  );
+};
+
+export default ApiFetch;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./globals.css";
+import ApiFetch from "./ApiFetch";
+
+const App = () => {
+  return (
+    <>
+      <ApiFetch />
+    </>
+  );
+};
+
+export default App;
